1ed5edec8ed600965430c769fd19f96ebfd4a15e
monica88lima <flima.monica@gmail.com>
2025-03-25 10:10:22 -0300

    Criando app angular

---
+++ Angular/MinhaAppAngular/src/app/componentes/navegacao/navegacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navegacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './navegacao.component.html',
+  styleUrl: './navegacao.component.scss'
+})
+export class NavegacaoComponent {
+
+}
